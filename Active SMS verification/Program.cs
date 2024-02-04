2149b24c7392e2c5fdc94c28ab1a144c8b5e2ebb
sajad <sajad.akhlaghi004@gmail.com>
2024-02-04 18:06:40 +0330

    fixid something

--- Active SMS verification/Program.cs
+++ Active SMS verification/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CreditCard card1 =new CreditCard();
+        card1.AddCard();
+        card1.ShowCards();
+       
+        
+    }
+}
+public class CreditCard
+{
+  
+  TextWriter CardNumber1 = new StreamWriter("K:\\sajad\\myRepository\\Active SMS verification\\CardList.txt");
+  
+public void AddCard()
+ {
+    System.Console.WriteLine("enter your card number (16 digits)");
+    var usercard = Console.ReadLine();
+    if(usercard.Length==16)
+    {
+        CardNumber1.WriteLine("Card number :" + usercard);
+    }
+    else{System.Console.WriteLine("error");}
+
+    System.Console.WriteLine("enter your name and family");
+    var userName = Console.ReadLine();
+    var userFamilly = Console.ReadLine();
+    CardNumber1.WriteLine("name : "+userName + " " + userFamilly);
+     //user Expiration will write in a .txt 
+     System.Console.WriteLine("enter your expirtion date with this format : yy/mm} example ==> 04/02");
+    var userED = Console.ReadLine();
+    if(userED.Substring(2,1)=="/")
+    {
+        CardNumber1.WriteLine("Expiration date : "+userED);
+        CardNumber1.Close();
+    }
+     else{System.Console.WriteLine("error");}
+
+ }
+ 
+ public void ShowCards()
+ {
+    string text =@"K:\\sajad\\myRepository\\Active SMS verification\\CardList.txt";
+    string t = File.ReadAllText(text);
+   Console.WriteLine("your info"+"\n"+t);
+ }
+}
--- Active SMS verification/obj/Debug/net8.0/Active SMS verification.AssemblyInfo.cs
+++ Active SMS verification/obj/Debug/net8.0/Active SMS verification.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Active SMS verification")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17628a7ab360de9c226a4a83c361ee664e4670c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8fe6974b5fb32f84f3aed31dc7e56ef4cb072ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Active SMS verification")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Active SMS verification")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
