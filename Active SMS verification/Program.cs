1d9b2140452e79651cad51e6838fe22814391784
sajad <sajad.akhlaghi004@gmail.com>
2024-02-05 10:42:23 +0330

    added Pos application

--- Active SMS verification/Program.cs
+++ Active SMS verification/Program.cs
@@ -1,49 +1,161 @@
+﻿using System;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         CreditCard card1 =new CreditCard();
-        card1.AddCard();
-        card1.ShowCards();
-       
-        
+        bool exit = false;
+        while (exit==false)
+        {
+          
+          System.Console.WriteLine("hi \n 1 = add new card \n 2 = show your card \n 3 = make dynamic code for purchase \n 4 = exit");
+          var UserSelect = Console.ReadLine();
+          switch(UserSelect)
+          {
+            case "1":
+            {
+              Console.Clear();
+              card1.AddCard();
+              System.Console.WriteLine("press any key for back to menu");
+              Console.ReadKey();
+              Console.Clear();
+              break;
+            }
+            case "2":
+            {
+              Console.Clear();
+              card1.ShowCards();
+               System.Console.WriteLine("press any key for back to menu");
+              Console.ReadKey();
+              Console.Clear();
+              break;
+            }
+            case "3":
+            {
+              Console.Clear();
+              card1.CodeMaker();
+              System.Console.WriteLine("press any key for back to menu");
+              Console.ReadKey();
+              Console.Clear();
+              break;
+            }
+            case "4":
+            {
+              Console.Clear();
+              System.Console.WriteLine("have a nice day");
+              exit=true;
+              break;
+            }
+
+
+          }
+        }
+      
     }
+
 }
 public class CreditCard
 {
-  
-  TextWriter CardNumber1 = new StreamWriter("K:\\sajad\\myRepository\\Active SMS verification\\CardList.txt");
+  TextWriter CardNumber = new StreamWriter("K:\\sajad\\myRepository\\Active SMS verification\\CardList.txt");
+  TextWriter NameAndFamily = new StreamWriter("K:\\sajad\\myRepository\\Active SMS verification\\NameAndFamily.txt");
+  TextWriter Cvv2 = new StreamWriter("K:\\sajad\\myRepository\\Active SMS verification\\cv2.txt");
+  TextWriter ExpirationDate = new StreamWriter("K:\\sajad\\myRepository\\Active SMS verification\\expiration.txt");
+  TextWriter DCode = new StreamWriter("K:\\sajad\\myRepository\\Active SMS verification\\dynamicCode.txt");
   
 public void AddCard()
- {
-    System.Console.WriteLine("enter your card number (16 digits)");
+ {  bool wrongInput = false;
+   if(wrongInput==false)
+   {
+     System.Console.WriteLine("enter your card number (16 digits)");
     var usercard = Console.ReadLine();
+    var alphabet = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'};
     if(usercard.Length==16)
     {
-        CardNumber1.WriteLine("Card number :" + usercard);
+       
+        CardNumber.WriteLine(usercard);
+        CardNumber.Close();
     }
-    else{System.Console.WriteLine("error");}
-
-    System.Console.WriteLine("enter your name and family");
-    var userName = Console.ReadLine();
-    var userFamilly = Console.ReadLine();
-    CardNumber1.WriteLine("name : "+userName + " " + userFamilly);
-     //user Expiration will write in a .txt 
-     System.Console.WriteLine("enter your expirtion date with this format : yy/mm} example ==> 04/02");
-    var userED = Console.ReadLine();
-    if(userED.Substring(2,1)=="/")
+    else
     {
-        CardNumber1.WriteLine("Expiration date : "+userED);
-        CardNumber1.Close();
+      System.Console.WriteLine("error");
+      wrongInput=true;
     }
-     else{System.Console.WriteLine("error");}
 
+    if (wrongInput==false)
+    {
+      System.Console.WriteLine("enter your name :");
+    var userName = Console.ReadLine();
+
+    System.Console.WriteLine("enter your family :");
+    var userFamilly = Console.ReadLine();
+   
+    NameAndFamily.WriteLine(userName + " " +  userFamilly);
+    NameAndFamily.Close();
+    System.Console.WriteLine("enter your cvv2 ( 3 <= cvv2 <= 5 ) ");
+    var cvv2 = Convert.ToInt32(Console.ReadLine());
+    if  (cvv2>99&&cvv2<999)
+    { 
+      Cvv2.WriteLine(cvv2);
+      Cvv2.Close();
+    }
+    else
+    {System.Console.WriteLine("error please enter cvv2 in correct form");
+      wrongInput = true;
+    }
+     //user Expiration will write in a .txt 
+    System.Console.WriteLine("enter your expiration year :");
+    var Expirationy = (Console.ReadLine());
+    var ExpirationYear = int.Parse(Expirationy);
+    
+    System.Console.WriteLine("enter your expiration month : ");
+    var ExpirationMonth = Convert.ToInt32(Console.ReadLine());
+
+    if(ExpirationYear<1500&&ExpirationYear>=1400)
+        {
+           if(ExpirationMonth<=12&&ExpirationMonth>=1)
+           {
+           ExpirationDate.WriteLine(ExpirationYear +"/"+ ExpirationMonth);
+            
+            ExpirationDate.Close();
+
+           }
+           
+                    
+        }
+        else 
+        { System.Console.WriteLine("please write date in correct form => example ( year :1401 month :12 ) "); 
+          wrongInput=true;
+        }
+        Console.Clear();
+        System.Console.WriteLine("your card info was saved succses !");
+   }
+    
+    
+   }
  }
  
  public void ShowCards()
  {
     string text =@"K:\\sajad\\myRepository\\Active SMS verification\\CardList.txt";
     string t = File.ReadAllText(text);
+    
    Console.WriteLine("your info"+"\n"+t);
  }
+public  void CodeMaker()
+{
+  System.Console.WriteLine("please wait a few ...");
+  
+  Random randomCode = new Random();
+  var result = randomCode.Next(100000,1000000);
+  DCode.WriteLine(result);
+  DCode.Close();
+  System.Console.WriteLine($"your code is : {result} \n dont share this code ! ");
+
+  
+
+
+
+}
+
 }
---
+++ pos/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PosDevice pos1 = new PosDevice();
+        
+        pos1.UserPrice();
+
+        pos1.UserCardNumer();
+        /*
+        pos1.Username();
+        pos1.Userfamily();
+        pos1.UserCvv2();
+        pos1.UserExpirationDate();
+        System.Console.WriteLine("please wait");
+        */
+       
+        
+    
+    }
+}
+public class PosDevice
+{
+    public int Price { get; set; }
+    public string CardNumber { get; set; }
+    public int CVV2 { get; set; }
+    public string Name { get; set; }
+    public string Family { get; set; }
+    public int expirationDate { get; set; }
+    public void UserPrice()
+    {
+    
+        System.Console.WriteLine("Please enter the price");
+        var userInput =Convert.ToInt32(Console.ReadLine());
+        Price = userInput;
+    }
+    public void UserCardNumer()
+    {
+        System.Console.WriteLine("please enter the card you have saved ");
+        var UserCardInput =Console.ReadLine(); 
+        CardNumber = UserCardInput ;
+    
+        StreamReader fileReader = new StreamReader($"K:\\sajad\\myRepository\\Active SMS verification\\CardList.txt");
+        var result = fileReader.ReadLine();
+        fileReader.Close();
+        
+
+
+
+    }
+    public void UserCvv2()
+    {
+        System.Console.WriteLine("enter your Cvv2");
+        var UserCvv2Input =Convert.ToInt32(Console.ReadLine());
+        CVV2 =UserCvv2Input;
+        
+    }
+    public void Username()
+    {
+        System.Console.WriteLine("enter your name");
+        var Uname = Console.ReadLine();
+          Name = Uname;
+    }
+    public void Userfamily()
+    {
+        System.Console.WriteLine("enter your family ");
+        var Ufamily = Console.ReadLine();
+        Family =  Ufamily;
+    }
+
+    public void UserExpirationDate()
+    {
+     
+        System.Console.WriteLine("enter year : like (1401) ");
+        int UExpirationYear =Convert.ToInt32(Console.ReadLine());
+
+         System.Console.WriteLine("enter Month : like (12) ");
+        int UExpirationMonth =Convert.ToInt32(Console.ReadLine());
+
+        var Date =UExpirationYear+UExpirationMonth;
+        expirationDate = Date;
+        
+    }
+
+    public virtual void Reading(string x)
+    {
+        StreamReader fileReader = new StreamReader($"K:\\sajad\\myRepository\\Active SMS verification\\{x.ToString()}+.txt");
+        
+        var result = fileReader.ReadLine();
+       
+    }
+   
+}
