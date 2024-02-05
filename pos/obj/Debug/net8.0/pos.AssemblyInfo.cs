049b5234c599196b29719c1aacfbd5899094ed4b
sajad <sajad.akhlaghi004@gmail.com>
2024-02-05 12:16:29 +0330

    some changes

--- pos/obj/Debug/net8.0/pos.AssemblyInfo.cs
+++ pos/obj/Debug/net8.0/pos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8129151d0814fa09315bbc58db34599d6e8b31a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24616ded2e68a03bce312e211980735087099115")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
